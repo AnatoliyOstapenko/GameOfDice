381c55148ba8a95cad6f92d464c2343dc24f5474
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-06-28 17:33:44 +0300

    randomElement() added
    made the app more neat

--- GameOfDice/ViewController.swift
+++ GameOfDice/ViewController.swift
@@ -10,18 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var diceLeftImageView: UIImageView!
     @IBOutlet weak var diceRightImageView: UIImageView!
-    var diceLeft = Int.random(in: 0...5)
-    var diceRight = Int.random(in: 0...5)
+    var diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceLeft = Int.random(in: 0...5)
-        diceRight = Int.random(in: 0...5)
-        twoDiceImageChanged()
-    }
-    
-    func twoDiceImageChanged() {
-        diceLeftImageView.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][diceLeft]
-        diceRightImageView.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][diceRight]
+        diceLeftImageView.image = diceArray.randomElement()
+        diceRightImageView.image = diceArray.randomElement()
+
     }
     
 }
